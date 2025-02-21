5f0f7133d6706789985a0fab50f04f0ce7c78440
dmytrolavrov <dimalavrov2004.06.16@gmail.com>
2025-02-21 18:28:37 +0200

    Updated server: added route connections (#5)

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
+import AdminRoutes from './routes/AdminRoutes.js';
+import SubscriberRoutes from './routes/SubscriberRoutes.js';
+import CityRoutes from './routes/CityRoutes.js';
+import CallRoutes from './routes/CallRoutes.js';
+
 dotenv.config();
 
 const port = process.env.PORT || 4444;
@@ -16,6 +21,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/auth', AdminRoutes);
+app.use('/subscribers', SubscriberRoutes);
+app.use('/cities', CityRoutes);
+app.use('/calls', CallRoutes);
 
 app.listen(port || 4444, (err) => {
   if (err) {
