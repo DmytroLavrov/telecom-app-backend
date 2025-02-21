2135024e85ec123bcab0f3e895ece0058d9b5c4c
dmytrolavrov <dimalavrov2004.06.16@gmail.com>
2025-02-21 17:20:54 +0200

    Add environment variables, MongoDB connection, and CORS middleware (#1)

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
-const port = 4444;
+dotenv.config();
+
+const port = process.env.PORT || 4444;
+
+mongoose
+  .connect(process.env.MONGO__URL)
+  .then(() => console.log('db ok'))
+  .catch((err) => console.log('db error', err));
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.listen(port || 4444, (err) => {
   if (err) {
